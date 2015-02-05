9ed6b6c85bad7a8e077a277334ea196685c74d46
Salvador Reyes <shavaa.reyes@gmail.com>
2015-02-05 09:53:18 -0600

    se quito los filtro no necesarios

--- actividad1_1.py
+++ actividad1_1.py
@@ -44,41 +44,6 @@ def escala_grises_menos(image):
             pic[i,j] = (R, G, B)
     return pic
 
-def filtro_umbral(image):
-    escala_grises(image)
-    pic = image.load()
-    for i in range(image.size[0]):
-        for j in range(image.size[1]):
- 
-            (R, G, B) = pic[i,j]
-            intensity = R
-            if intensity < 128:
-                intensity = 0
-            else:
-                intensity = 255
-            R = G = B = intensity
-            pic[i,j] = (R, G, B)
-    return pic
- 
-def filtro_media(image):
-    escala_grises(image)
-    pic = image.load()
-    pic_copy = (image.copy()).load()
- 
-    for i in range(image.size[0]):
-        for j in range(image.size[1]):
- 
-            temp = []
-            for h in range(i-1, i+2):
-                for l in range(j-1, j+2):
-                    if h >= 0 and l >= 0 and h < image.size[0] and l < image.size[1]:
-                        temp.append(pic_copy[i, j][0])
- 
-            temp.sort()
-            R = G = B = int(temp[int(len(temp)/2)])
-            pic[i,j] = (R, G, B)
-    return pic
- 
 efecto = {
     "grayscale":escala_grises,
     "grises_mas":escala_grises_mas,
